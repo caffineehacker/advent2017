a802a8242cc799a1f7ae92cf96c9a85473264d52
Tim Waterhouse <tim@timwaterhouse.com>
2023-11-21 19:45:03 -0800

    Day 2, part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -38,4 +38,17 @@ fn main() {
         .map(|line| line.iter().max().unwrap() - line.iter().min().unwrap())
         .sum();
     println!("Part 1: {}", result1);
+
+    let result2: i32 = split_lines
+        .iter()
+        .map(|line| {
+            let good_pair = line
+                .iter()
+                .permutations(2)
+                .find(|pair| pair[0].max(pair[1]) % pair[0].min(pair[1]) == 0)
+                .unwrap();
+            good_pair[0].max(good_pair[1]) / good_pair[0].min(good_pair[1])
+        })
+        .sum();
+    println!("Part 2: {}", result2)
 }
