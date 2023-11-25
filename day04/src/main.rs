6123f2615308c0eb239f8e1292689ce5663ae319
Tim Waterhouse <tim@timwaterhouse.com>
2023-11-25 12:43:17 -0800

    Day 4, part 1

---
+++ day04/src/main.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    data_file: String,
+    #[arg(long)]
+    debug: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let file = File::open(&args.data_file).expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader
+        .lines()
+        .map(|line| line.expect("Failed to read line"))
+        .collect();
+
+    let valid_lines: Vec<&String> = lines
+        .iter()
+        .filter(|line| line.split_ascii_whitespace().all_unique())
+        .collect();
+
+    println!("Part 1: {}", valid_lines.len());
+}
