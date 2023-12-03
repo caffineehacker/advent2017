43075c7898e060f8ad16e3756a04d54b33ea6dc7
Tim Waterhouse <tim@timwaterhouse.com>
2023-12-03 13:31:33 -0800

    Day 10, part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -26,31 +26,67 @@ fn main() {
         .map(|line| line.expect("Failed to read line"))
         .collect();
 
+    let instructions = lines
+        .get(0)
+        .unwrap()
+        .split(',')
+        .map(|instruction| instruction.parse::<usize>().unwrap())
+        .collect_vec();
+    let part1_string = do_knot(instructions, 1, args.debug);
+
+    if args.debug {
+        part1_string.iter().for_each(|n| print!("{}, ", n));
+        println!("");
+    }
+
+    println!("Part 1: {}", part1_string[0] * part1_string[1]);
+
+    let mut part2_instructions = lines
+        .get(0)
+        .unwrap()
+        .chars()
+        .map(|c| c as u8 as usize)
+        .collect_vec();
+    part2_instructions.append(&mut vec![17, 31, 73, 47, 23]);
+    let part2_string = do_knot(part2_instructions, 64, args.debug);
+    let mut dense_hash = Vec::new();
+    for i in 0..16 {
+        let mut value = 0;
+        for j in 0..16 {
+            value ^= part2_string[i * 16 + j];
+        }
+        dense_hash.push(value);
+    }
+
+    print!("Part 2: ");
+    for entry in dense_hash.iter() {
+        print!("{:2x}", entry);
+    }
+    println!("");
+}
+
+fn do_knot(instructions: Vec<usize>, rounds: i32, debug: bool) -> Vec<i32> {
     let mut string = (0..256).collect_vec();
     let mut index = 0;
     let mut skip = 0;
 
-    for instruction in lines.get(0).unwrap().split(',') {
-        let instruction = instruction.parse::<usize>().unwrap();
-        for i in 0..(instruction / 2) {
-            string.swap((i + index) % 256, (instruction - i - 1 + index) % 256);
-        }
+    for _ in 0..rounds {
+        for instruction in instructions.iter() {
+            for i in 0..(instruction / 2) {
+                string.swap((i + index) % 256, (instruction - i - 1 + index) % 256);
+            }
 
-        if args.debug {
-            println!("Instr: {}, skip: {}, index: {}", instruction, skip, index);
-            string.iter().for_each(|n| print!("{}, ", n));
-            println!("");
-        }
+            if debug {
+                println!("Instr: {}, skip: {}, index: {}", instruction, skip, index);
+                string.iter().for_each(|n| print!("{}, ", n));
+                println!("");
+            }
 
-        index += instruction + skip;
-        index %= 256;
-        skip += 1;
+            index += instruction + skip;
+            index %= 256;
+            skip += 1;
+        }
     }
 
-    if args.debug {
-        string.iter().for_each(|n| print!("{}, ", n));
-        println!("");
-    }
-
-    println!("Part 1: {}", string[0] * string[1])
+    string
 }
